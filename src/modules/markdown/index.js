672b248875938d53823e808abc4a43ecbc49f1e4
z.cius <zr.cius@gmail.com>
2018-10-11 00:35:06 +0800

    handleInput on pattern side

--- src/modules/markdown/index.js
+++ src/modules/markdown/index.js
@@ -9,21 +9,14 @@ export default class Markdown extends Module {
         this.patterns = mergePatterns(this.options.patterns);
     }
 
-    matchPatterns (meta) {
-        return this.patterns.filter(p => p.match(meta));
-    }
-
     [editorEvents.get('input')] (change, event, editor) {
-        const { keys, nodes, blocks, block, offset } = change.selection.focus;
-        const first = edgeLeafOf(blocks[0], { tail: false });
+        const focus = change.selection.focus;
+        const first = edgeLeafOf(focus.blocks[0], { tail: false });
+        const meta = { ...focus, first, node: focus.nodes[0] };
 
-        const meta = {
-            keys, nodes, blocks, offset,
-            block, first, node: nodes[0]
-        };
-
-        for (const pattern of this.matchPatterns(meta)) {
-            if (pattern.handle(change, meta, editor, event)) break;
+        for (const pattern of this.patterns) {
+            if (typeof pattern.handleInput === 'function' &&
+                pattern.handleInput(change, meta, editor, event)) break;
         }
     }
 }
