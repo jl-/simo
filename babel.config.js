a44c461d339f39d4b749d7e03d8f0f98dd991333
z.cius <zr.cius@gmail.com>
2018-10-10 14:21:22 +0800

    init babel config

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+const presets = ['@babel/env'];
+
+const plugins = [
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-proposal-class-properties'
+];
+
+module.exports = { presets, plugins };
