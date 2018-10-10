f0d03d79a6efc0abbdf7783f468afbf82fac1621
z.cius <zr.cius@gmail.com>
2018-10-10 17:14:33 +0800

    remove static styles

--- src/renderer/shell.vue
+++ src/renderer/shell.vue
@@ -1,10 +1,3 @@
 <template>
-    <div class="editor" :class="$.root" data-gramm="false" />
+    <div class="editor" data-gramm="false" />
 </template>
-
-<style lang="scss" module="$">
-    .root {
-        min-height: 200px;
-        max-height: 600%;
-    }
-</style>
