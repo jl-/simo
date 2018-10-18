84381c237f90c5877e24bee3dbb9f853b0e37aa3
z.cius <zr.cius@gmail.com>
2018-10-18 14:06:29 +0800

    merge formats

--- src/modules/markdown/index.js
+++ src/modules/markdown/index.js
@@ -1,12 +1,21 @@
 import Module from '../base';
-import { mergePatterns } from './options';
+import { mergeFormats } from './options';
 import { edgeLeafOf } from '../../utils/node';
-import { editorEvents } from '../../meta/events';
+import { editorEvents, editorHooks } from '../../meta/events';
 
 export default class Markdown extends Module {
-    constructor (options = {}, editor) {
-        super(options, editor);
-        this.patterns = mergePatterns(this.options.patterns);
+    constructor (editor, options = {}) {
+        super(editor, options);
+        this.formats = new Map();
+
+        editor.hook(editorHooks.BEFORE_MOUNT, () => {
+            const formats = mergeFormats(options.formats);
+            for (const name of Object.keys(formats)) {
+                if (formats[name] && editor.schema.supports(name)) {
+                    this.formats.set(name, formats[name]);
+                }
+            }
+        });
     }
 
     [editorEvents.get('input')] (change, event, editor) {
@@ -14,9 +23,9 @@ export default class Markdown extends Module {
         const first = edgeLeafOf(focus.blocks[0], { tail: false });
         const meta = { ...focus, first, node: focus.nodes[0] };
 
-        for (const pattern of this.patterns) {
-            if (typeof pattern.handleInput === 'function' &&
-                pattern.handleInput(change, meta, editor, event)) break;
+        for (const format of this.formats.values()) {
+            if (typeof format.handleInput === 'function' &&
+                format.handleInput(change, meta, editor, event)) break;
         }
     }
 }
--- src/modules/markdown/options.js
+++ src/modules/markdown/options.js
@@ -1,11 +1,21 @@
-import * as List from './list';
-import * as Heading from './heading';
-import * as Quotation from './quotation';
+import * as list from './list';
+import * as heading from './heading';
+import * as blockquote from './blockquote';
 
-export const builtinPatterns = [
-    List, Heading, Quotation
-];
+export const builtinFormats = {
+    // list, heading, blockquote
+    list, heading
+};
 
-export function mergePatterns (patterns) {
-    return patterns || builtinPatterns;
+export function mergeFormats (formats = {}) {
+    const result = { ...builtinFormats };
+
+    const ts = Object.prototype.toString;
+    for (const name of Object.keys(formats)) {
+        const format = formats[name];
+        const isObj = ts.call(format) === '[object Object]';
+        result[name] = isObj ? { ...result[name], ...format } : null;
+    }
+
+    return result;
 }
