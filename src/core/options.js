b16bf7779e46afc88b838003c5ac27c86589c9e2
z.cius <zr.cius@gmail.com>
2018-10-18 14:01:28 +0800

    merge schema options

--- src/core/options.js
+++ src/core/options.js
@@ -1,11 +1,9 @@
-import * as builtinFmts from '../formats';
-import { schema as builtinSchema } from '../meta/node';
+import { sanitizeSchema } from '../schema';
 
 export default function sanitize (options = {}) {
     const modules = options.modules || {};
     const readonly = Boolean(options.readonly);
-    const formats = options.formats || builtinFmts;
-    const schema = options.schema || builtinSchema;
+    const schema = sanitizeSchema(options.schema);
 
-    return Object.freeze({ ...options, schema, formats, modules, readonly });
+    return Object.freeze({ ...options, schema, modules, readonly });
 }
