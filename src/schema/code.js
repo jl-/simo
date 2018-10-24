825e93aa3e006c73d8df658c28c51faf2ad7d7ad
z.cius <zr.cius@gmail.com>
2018-10-24 11:49:39 +0800

    TODO: break on empty last linefeed

--- src/schema/code.js
+++ src/schema/code.js
@@ -1,12 +1,8 @@
 import * as actions from '../meta/actions';
 
 export function lineFeedLeaf (change, focus) {
-    const text = focus.nodes[0].text;
-    const isAtEnd = focus.offset === text.length ||
-        /^\u200B?$/.test(text.slice(focus.offset));
-    if (isAtEnd && /(\n\u200B?){2}$/.test(text.slice(0, focus.offset))) {
-        console.log('xx');
-    } else {
-        change[actions.REPLACE_TEXT](focus, '\n');
-    }
+    // const text = focus.nodes[0].text;
+    // const isAtEnd = focus.offset === text.length ||
+    //     /^\n?$/.test(text.slice(focus.offset));
+    change[actions.REPLACE_TEXT](focus, '\n');
 }
